5867a15da11399079ec955acff89699a54746461
24981a05qx-sudo <24981a05qx@raghuenggcollege.in>
2026-01-25 15:45:03 +0530

    Create python27.py
    problem solved
    LEVEL : EASY
    
    LEFT ROTATION OF GIVRN ARRAY BY ONE PLACE

---
+++ python27.py
@@ -0,0 +1,38 @@
+#Given an integer array nums, rotate the array to the left by one.
+
+
+
+#Note: There is no need to return anything, just modify the given array.
+
+
+#Example 1
+
+#Input: nums = [1, 2, 3, 4, 5]
+#Output: [2, 3, 4, 5, 1]
+#Explanation:
+
+#Initially, nums = [1, 2, 3, 4, 5]
+#Rotating once to left -> nums = [2, 3, 4, 5, 1]
+
+#Example 2
+
+#Input: nums = [-1, 0, 3, 6]
+#Output: [0, 3, 6, -1]
+#Explanation:
+
+#Initially, nums = [-1, 0, 3, 6]
+#Rotating once to left -> nums = [0, 3, 6, -1]
+
+
+
+
+arr=list(map(int,input("Enter the array elements:").split()))
+n=len(arr)
+first=arr[0]
+
+for i in range(1,n):
+    arr[i-1]=arr[i]
+
+arr[n-1]=first
+
+print("The array after single left rotation is :",arr)
